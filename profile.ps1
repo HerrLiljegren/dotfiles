8f46b37a060714175187d9d1b53d902465c1628f
HerrLiljegren <jesper.liljegren@gmail.com>
2020-06-21 09:53:04 +0200

    Update profile.ps1
    Change how new line suffix is used in posh-git

--- profile.ps1
+++ profile.ps1
@@ -1,5 +1,4 @@
 $env:LC_ALL='C.UTF-8'
 #Import-WslCommand "awk", "emacs", "grep", "head", "less", "ls", "man", "sed", "seq", "ssh", "tail", "vim", "vi", "htop", "find"
 Import-Module posh-git
-$GitPromptSettings.DefaultPromptBeforeSuffix.Text = '`n'
-New-Alias g git
+$GitPromptSettings.DefaultPromptSuffix = '`n$(''>'' * ($nestedPromptLevel + 1)) '
