92e6a2c03483d44835d5cf1b95a3b0624a3f2c20
Jesper Liljegren <jesper.liljegren@gmail.com>
2026-07-31 14:52:45 +0200

    feat(nvim): configure prettier formatting

---
+++ config/nvim/lua/plugins/formatting.lua
@@ -0,0 +1,15 @@
+return {
+  {
+    "stevearc/conform.nvim",
+    opts = {
+      formatters_by_ft = {
+        cs = { "csharpier" },
+        markdown = { "prettier" },
+        javascript = { "prettier" },
+        javascriptreact = { "prettier" },
+        typescript = { "prettier" },
+        typescriptreact = { "prettier" },
+      },
+    },
+  },
+}
