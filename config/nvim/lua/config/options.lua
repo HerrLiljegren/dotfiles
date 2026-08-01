febda27e0416b460d16514096c710db58d44319b
Jesper Liljegren <jesper.liljegren@gmail.com>
2026-08-01 11:10:22 +0200

    feat(nvim): enable osc 52 clipboard over ssh

--- config/nvim/lua/config/options.lua
+++ config/nvim/lua/config/options.lua
@@ -15,3 +15,19 @@ vim.opt.splitright = true
 vim.opt.tabstop = 2
 vim.opt.termguicolors = true
 vim.opt.undofile = true
+
+if vim.env.SSH_CONNECTION then
+  local osc52 = require("vim.ui.clipboard.osc52")
+
+  vim.g.clipboard = {
+    name = "OSC 52",
+    copy = {
+      ["+"] = osc52.copy("+"),
+      ["*"] = osc52.copy("*"),
+    },
+    paste = {
+      ["+"] = osc52.paste("+"),
+      ["*"] = osc52.paste("*"),
+    },
+  }
+end
