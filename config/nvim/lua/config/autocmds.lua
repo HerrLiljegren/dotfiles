c79e6b22dde5717718763d804b9ce6a08ad7336a
Jesper Liljegren <jesper.liljegren@gmail.com>
2026-08-13 12:45:46 +0200

    feat(nvim): disable wrapping for markdown

--- config/nvim/lua/config/autocmds.lua
+++ config/nvim/lua/config/autocmds.lua
@@ -1,1 +1,6 @@
--- Add shared autocommands here when they are needed.
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = { "markdown", "markdown.mdx" },
+  callback = function()
+    vim.opt_local.wrap = false
+  end,
+})
