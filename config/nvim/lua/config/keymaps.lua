3da497fd95819bb0e39f66736614ba4762e0e5e2
Jesper Liljegren <jesper.liljegren@gmail.com>
2026-08-13 12:45:46 +0200

    feat(nvim): map shifted wheel to horizontal scroll

--- config/nvim/lua/config/keymaps.lua
+++ config/nvim/lua/config/keymaps.lua
@@ -1,2 +1,4 @@
 vim.keymap.set("n", "<leader>w", "<cmd>write<cr>", { desc = "Write file" })
 vim.keymap.set("n", "<leader>q", "<cmd>quit<cr>", { desc = "Quit window" })
+vim.keymap.set({ "n", "x", "i" }, "<S-ScrollWheelUp>", "<ScrollWheelLeft>")
+vim.keymap.set({ "n", "x", "i" }, "<S-ScrollWheelDown>", "<ScrollWheelRight>")
