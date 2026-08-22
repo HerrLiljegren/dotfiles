d2f5cf2fe79814eb456f6f5864c8b90f10071802
Jesper Liljegren <jesper.liljegren@gmail.com>
2026-08-22 13:35:37 +0200

    feat(nvim): add kdl formatting

--- config/nvim/lua/plugins/formatting.lua
+++ config/nvim/lua/plugins/formatting.lua
@@ -5,6 +5,7 @@ return {
       formatters_by_ft = {
         cs = { "csharpier" },
         markdown = { "prettier" },
+        kdl = { "kdlfmt" },
         javascript = { "prettier" },
         javascriptreact = { "prettier" },
         typescript = { "prettier" },
