d6445fcf0283262f40380ca2d69105dd31c3d70c
Jesper Liljegren <jesper.liljegren@gmail.com>
2026-08-22 14:42:02 +0200

    fix(nvim): remove unavailable kdl formatter

--- config/nvim/lua/plugins/formatting.lua
+++ config/nvim/lua/plugins/formatting.lua
@@ -5,7 +5,6 @@ return {
       formatters_by_ft = {
         cs = { "csharpier" },
         markdown = { "prettier" },
-        kdl = { "kdlfmt" },
         javascript = { "prettier" },
         javascriptreact = { "prettier" },
         typescript = { "prettier" },
